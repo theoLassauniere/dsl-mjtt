475dea4d097bd15028f8f96694c081c82e0eedf2
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-12-01 08:51:58 +0100

    feat: dual check

---
+++ externals/langium/demo/generated/DualCheck.ino
@@ -0,0 +1,50 @@
+
+//Wiring code generated from an ArduinoML model
+// Application name: DualCheck
+
+long debounce = 200;
+enum STATE {off, on};
+
+STATE currentState = off;
+
+bool button1BounceGuard = false;
+long button1LastDebounceTime = 0;
+
+            
+
+bool button2BounceGuard = false;
+long button2LastDebounceTime = 0;
+
+            
+
+	void setup(){
+		pinMode(12, OUTPUT); // red_led [Actuator]
+		pinMode(9, INPUT); // button1 [Sensor]
+		pinMode(8, INPUT); // button2 [Sensor]
+	}
+	void loop() {
+			switch(currentState){
+
+				case off:
+					digitalWrite(12,LOW);
+		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
+        
+            if( (digitalRead(9) == HIGH && button1BounceGuard) && (digitalRead(8) == HIGH && button2BounceGuard)  {
+                button1LastDebounceTime = millis();
+                button2LastDebounceTime = millis();
+                currentState = on;
+        
+				break;
+				case on:
+					digitalWrite(12,HIGH);
+		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
+        
+            if( (digitalRead(9) == LOW && button1BounceGuard) || (digitalRead(8) == LOW && button2BounceGuard)  {
+                button1LastDebounceTime = millis();
+                button2LastDebounceTime = millis();
+                currentState = off;
+        
+				break;
+		}
+	}
+	
