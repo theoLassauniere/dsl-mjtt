d39a6bfc47318e00c0a16cc3e96906174bf6af93
thibo24 <thibault.ripoche@free.fr>
2025-12-06 13:31:14 +0100

    WIP : ADDING DELAY

--- embeddeds/groovy/GroovuinoML/demo/ExceptionThrowing.groovy
+++ embeddeds/groovy/GroovuinoML/demo/ExceptionThrowing.groovy
@@ -1,24 +1,26 @@
-
 sensor "button1" onPin 9
-
 sensor "button2" onPin 8
 
 actuator "errorLed" pin 12
+actuator "greenLed" pin 11
 
-errorState "error3", 3
+errorState "error", 3
 
-state "off" means "errorLed" becomes "low"
-
-state "on" means "errorLed" becomes "high"
+state "off" means "greenLed" becomes "low"
+state "on"  means "greenLed" becomes "high"
 
 initial "off"
 
-from "off" to "error3" when "button1" becomes "high" and "button2" becomes "high"
-from "off" to "on"     when "button1" becomes "high"
-from "off" to "on"     when "button2" becomes "high"
+// transitions depuis off
+// priorité : si les deux boutons sont appuyés en même temps -> erreur (blocage)
+from "off" to "error" when "button1" becomes "high" and "button2" becomes "high"
 
-from "on" to "error3" when "button1" becomes "high" and "button2" becomes "high"
-from "on" to "off"    when "button1" becomes "low"  and "button2" becomes "high"
-from "on" to "off"    when "button2" becomes "low"  and "button1" becomes "high"
+// utilisation exclusive : un seul bouton à la fois allume l'état ON (OR)
+from "off" to "on" when "button1" becomes "high" or "button2" becomes "high"
+
+// transitions depuis on
+from "on" to "error" when "button1" becomes "high" and "button2" becomes "high"
+// revenir à off quand un des boutons est relâché (OR)
+from "on" to "off" when "button1" becomes "low" or "button2" becomes "low"
 
 export "ExceptionThrowing"
--- embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLBasescript.groovy
+++ embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLBasescript.groovy
@@ -9,54 +9,51 @@ import io.github.mosser.arduinoml.kernel.structural.Sensor
 import io.github.mosser.arduinoml.kernel.structural.SIGNAL
 
 abstract class GroovuinoMLBasescript extends Script {
-//	public static Number getDuration(Number number, TimeUnit unit) throws IOException {
-//		return number * unit.inMillis;
-//	}
 
     // sensor "name" pin n
     def sensor(String name) {
         [
-                pin  : { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createSensor(name, n) },
-                onPin: { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createSensor(name, n) }
+            pin  : { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createSensor(name, n) },
+            onPin: { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createSensor(name, n) }
         ]
     }
 
     // actuator "name" pin n
     def actuator(String name) {
         [
-                pin: { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createActuator(name, n) }
+            pin: { n -> ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createActuator(name, n) }
         ]
     }
 
-    // state "name" means actuator becomes signal [and actuator becomes signal]\*n
+    // state "name" means actuator becomes signal [and actuator becomes signal]*n
     def state(String name) {
         List<Action> actions = new ArrayList<Action>()
-        ((GroovuinoMLBinding) this.getBinding())
-                .getGroovuinoMLModel()
-                .createState(name, actions)
+        ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createState(name, actions)
 
         // recursive closure to allow multiple and statements
         def closure
         closure = { actuator ->
             [
-                    becomes: { signal ->
-                        Action action = new Action()
-                        action.setActuator(
-                                actuator instanceof String
-                                        ? (Actuator) ((GroovuinoMLBinding) this.getBinding()).getVariable(actuator)
-                                        : (Actuator) actuator
-                        )
-                        action.setValue(
-                                signal instanceof String
-                                        ? (SIGNAL) ((GroovuinoMLBinding) this.getBinding()).getVariable(signal)
-                                        : (SIGNAL) signal
-                        )
-                        actions.add(action)
-                        [and: closure]
-                    }
+                becomes: { signal ->
+                    Action action = new Action()
+                    action.setActuator(
+                        actuator instanceof String
+                            ? (Actuator) ((GroovuinoMLBinding) this.getBinding()).getVariable(actuator)
+                            : (Actuator) actuator
+                    )
+                    action.setValue(
+                        signal instanceof String
+                            ? (SIGNAL) ((GroovuinoMLBinding) this.getBinding()).getVariable(signal)
+                            : (SIGNAL) signal
+                    )
+                    actions.add(action)
+                    [and: closure]
+                }
             ]
-        }[means: closure]
-	}
+        }
+
+        return [means: closure]
+    }
 
     def errorState(String name, int times, String actuatorName = "errorLed") {
         List<Action> actions = new ArrayList<Action>()
@@ -80,90 +77,95 @@ abstract class GroovuinoMLBasescript extends Script {
         ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createState(name, actions)
     }
 
-	// initial state
-	def initial(state) {
-		((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().setInitialState(state instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state) : (State)state
-                )
+    // initial state
+    def initial(state) {
+        ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().setInitialState(
+            state instanceof String ? (State) ((GroovuinoMLBinding) this.getBinding()).getVariable(state) : (State) state
+        )
     }
 
     // from state1 to state2 when s1 becomes v1 and/or s2 becomes v2 ...
     def from(state1) {
         [
-                to: { state2 ->
-                    def model = ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel()
+            to: { state2 ->
+                def model = ((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel()
 
-                    State fromState = state1 instanceof String
-                            ? (State) ((GroovuinoMLBinding) this.getBinding()).getVariable(state1)
-                            : (State) state1
+                State fromState = state1 instanceof String
+                        ? (State) ((GroovuinoMLBinding) this.getBinding()).getVariable(state1)
+                        : (State) state1
 
-                    State toState = state2 instanceof String
-                            ? (State) ((GroovuinoMLBinding) this.getBinding()).getVariable(state2)
-                            : (State) state2
+                State toState = state2 instanceof String
+                        ? (State) ((GroovuinoMLBinding) this.getBinding()).getVariable(state2)
+                        : (State) state2
 
-                    SignalTransition transitionRef = null
+                SignalTransition transitionRef = null
 
-                    def resolveSensor = { s ->
-                        s instanceof String
-                                ? (Sensor) ((GroovuinoMLBinding) this.getBinding()).getVariable(s)
-                                : (Sensor) s
-                    }
-                    def resolveSignal = { v ->
-                        v instanceof String
-                                ? (SIGNAL) ((GroovuinoMLBinding) this.getBinding()).getVariable(v)
-                                : (SIGNAL) v
-                    }
+                def resolveSensor = { s ->
+                    s instanceof String
+                            ? (Sensor) ((GroovuinoMLBinding) this.getBinding()).getVariable(s)
+                            : (Sensor) s
+                }
+                def resolveSignal = { v ->
+                    v instanceof String
+                            ? (SIGNAL) ((GroovuinoMLBinding) this.getBinding()).getVariable(v)
+                            : (SIGNAL) v
+                }
 
-                    // fonction interne pour ajouter une condition AND ou OR
-                    def doAddCondition = { sensor, signal, boolean isOr ->
-                        Sensor s = resolveSensor(sensor)
-                        SIGNAL v = resolveSignal(signal)
+                // fonction interne pour ajouter une condition AND ou OR
+                def doAddCondition = { sensor, signal, boolean isOr ->
+                    Sensor s = resolveSensor(sensor)
+                    SIGNAL v = resolveSignal(signal)
 
-                        if (transitionRef == null) {
-                            transitionRef = model.createSignalTransition(fromState, toState, s, v)
+                    if (transitionRef == null) {
+                        transitionRef = model.createSignalTransition(fromState, toState, s, v)
+                    } else {
+                        if (isOr) {
+                            transitionRef.addConditionOr(s, v)
                         } else {
-                            transitionRef.addCondition(s, v, isOr)
+                            transitionRef.addCondition(s, v)
                         }
                     }
+                }
 
-                    // on déclare d'abord les closures and / or,
-                    // puis on les utilise dans addCondition
-                    def andClosure
-                    def orClosure
+                // on déclare d'abord les closures and / or,
+                // puis on les utilise dans addCondition
+                def andClosure
+                def orClosure
 
-                    andClosure = { sensor ->
-                        [
-                                becomes: { signal ->
-                                    doAddCondition(sensor, signal, false)
-                                    [
-                                            and: andClosure,
-                                            or : orClosure
-                                    ]
-                                }
-                        ]
-                    }
-
-                    orClosure = { sensor ->
-                        [
-                                becomes: { signal ->
-                                    doAddCondition(sensor, signal, true)
-                                    [
-                                            and: andClosure,
-                                            or : orClosure
-                                    ]
-                                }
-                        ]
-                    }
-
-                    // point d'entrée pour le DSL: when ...
-                    def addCondition = andClosure
-
+                andClosure = { sensor ->
                     [
-                            when : addCondition,
-                            after: { delay ->
-                                model.createTimeTransition(fromState, toState, delay)
-                            }
+                        becomes: { signal ->
+                            doAddCondition(sensor, signal, false)
+                            [
+                                and: andClosure,
+                                or : orClosure
+                            ]
+                        }
                     ]
                 }
+
+                orClosure = { sensor ->
+                    [
+                        becomes: { signal ->
+                            doAddCondition(sensor, signal, true)
+                            [
+                                and: andClosure,
+                                or : orClosure
+                            ]
+                        }
+                    ]
+                }
+
+                // point d'entrée pour le DSL: when ...
+                def addCondition = andClosure
+
+                [
+                    when : addCondition,
+                    after: { delay ->
+                        model.createTimeTransition(fromState, toState, delay)
+                    }
+                ]
+            }
         ]
     }
 
